115c8262d3c23f0ce7c33e4c7784fdb9ed251720
1option <mahagon.here@gmail.com>
2023-09-17 19:12:41 +0300

    updated via magit

--- .emacs.d/early-init.el
+++ .emacs.d/early-init.el
@@ -1,2 +1,24 @@
+;;; init.el --- -*- lexical-binding: t -*-
+;;  Author: Maksim Rozhkov
+;;; Commentary:
+;;  This is my personal Emacs configuration
+;;; Code:
+
+(menu-bar-mode -1)
+(unless (and (display-graphic-p) (eq system-type 'darwin))
+  (push '(menu-bar-lines . 0) default-frame-alist))
+(push '(tool-bar-lines . 0) default-frame-alist)
+(push '(vertical-scroll-bars) default-frame-alist)
+
+(setq gc-cons-threshold most-positive-fixnum
+      gc-cons-percentage 0.6
+      file-name-handler-alist nil
+      site-run-file nil
+      read-process-output-max (* 10 1024 1024)
+      bidi-inhibit-bpa t)
+
 ;; Disable package.el in favor of straight.el
 (setq package-enable-at-startup nil)
+
+(provide 'early-init)
+;;; early-init.el ends here
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,4 +1,9 @@
-;; Load cutom-variables
+;;; init.el --- -*- lexical-binding: t -*-
+;;  Author: Maksim Rozhkov
+;;; Commentary:
+;;  This is my personal Emacs configuration
+;;; Code:
+
 (setq custom-file "~/.emacs.d/emacs-custom.el")
 (load custom-file)
 
@@ -6,36 +11,31 @@
 
 (require 'package)
 (add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages/") t)
+            '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
 ;; Bootstrap 'use-package'
-(eval-after-load 'gnutls
-  '(add-to-list 'gnutls-trustfiles "/etc/ssl/cert.pem"))
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
-(eval-when-compile
-  (require 'use-package))
-(require 'bind-key)
-(setq use-package-always-ensure t)
+(eval-and-compile
+  (setq use-package-always-ensure t
+        use-package-expand-minimally t))
 
 ;; Options
 
 ;; Minimize garbage collection during startup
 (setq gc-cons-threshold most-positive-fixnum)
-
 (setq inhibit-startup-echo-area-message t)
 (setq inhibit-startup-message t)
-
 (save-place-mode 1)
 (menu-bar-mode -1)
 ;; auto close bracket insertion. New in emacs 24
 (electric-pair-mode 1)
 ;; make electric-pair-mode work on more brackets
 (setq electric-pair-pairs
-       '(
-         (?\" . ?\")
+      '(
+        (?\" . ?\")
  	(?{ . ?\})))
 (global-display-line-numbers-mode 1)
 ;; Revert buffers when the underlying file has changed
@@ -47,16 +47,21 @@
 (recentf-mode 1)
 (global-set-key (kbd "\C-xf") 'recentf-open-files)
 
-;; ox-md
-(require 'ox-md)
+;; gcmh
+(use-package gcmh
+  ;; :hook (emacs-startup-hook . gcmh-mode)
+  :demand t
+  :config
+  (setq gcmh-low-cons-threshold (* 16 1024 1024))
+  (gcmh-mode +1))
 
 ;; mood-line
-(use-package mood-line
+;; (use-package mood-line
 
-  ;; Enable mood-line
-  :config
-  (mood-line-mode)):
-(setq mood-line-glyph-alist mood-line-glyphs-unicode)
+;; Enable mood-line
+;; :config
+;; (mood-line-mode)):
+;; (setq mood-line-glyph-alist mood-line-glyphs-unicode)
 
 ;; vertico
 (use-package vertico
@@ -67,27 +72,233 @@
   )
 
 ;; Optionally use the `orderless' completion style.
-(use-package orderless
+;; (use-package orderless
+;; :init
+;; Configure a custom style dispatcher (see the Consult wiki)
+;; (setq orderless-style-dispatchers '(+orderless-consult-dispatch orderless-affix-dispatch)
+;;       orderless-component-separator #'orderless-escapable-split-on-space)
+;; (setq completion-styles '(orderless basic)
+;; completion-category-defaults nil
+;; completion-category-overrides '((file (styles partial-completion)))))
+
+;;(setq org-clock-sound "~/Documents/emacs/ding.wav")
+
+(use-package lsp-mode
   :init
-  ;; Configure a custom style dispatcher (see the Consult wiki)
-  ;; (setq orderless-style-dispatchers '(+orderless-consult-dispatch orderless-affix-dispatch)
-  ;;       orderless-component-separator #'orderless-escapable-split-on-space)
-  (setq completion-styles '(orderless basic)
-        completion-category-defaults nil
-        completion-category-overrides '((file (styles partial-completion)))))
+  (setq lsp-keymap-prefix "C-c l")
+  :hook ((python-mode
+	  ;; java-mode
+	  js-mode
+	  js-jsx-mode
+	  typescript-mode
+	  web-mode
+	  ) . lsp-deferred)
+  :commands lsp
+  :config
+  (add-hook 'java-mode-hook #'(lambda () (when (eq major-mode 'java-mode) (lsp-deferred))))
+  (global-unset-key (kbd "<f2>"))
+  (define-key lsp-mode-map (kbd "<f2>") #'lsp-rename)
+  (setq lsp-auto-guess-root t)
+  (setq lsp-log-io nil)
+  (setq lsp-restart 'auto-restart)
+  (setq lsp-enable-links nil)
+  (setq lsp-enable-symbol-highlighting nil)
+  (setq lsp-enable-on-type-formatting nil)
+  (setq lsp-lens-enable nil)
+  (setq lsp-signature-auto-activate nil)
+  (setq lsp-signature-render-documentation nil)
+  (setq lsp-eldoc-enable-hover nil)
+  (setq lsp-eldoc-hook nil)
+  (setq lsp-modeline-code-actions-enable nil)
+  (setq lsp-modeline-diagnostics-enable nil)
+  (setq lsp-headerline-breadcrumb-enable nil)
+  (setq lsp-headerline-breadcrumb-icons-enable nil)
+  (setq lsp-semantic-tokens-enable nil)
+  (setq lsp-enable-folding nil)
+  (setq lsp-enable-imenu nil)
+  (setq lsp-enable-snippet nil)
+  (setq lsp-enable-file-watchers nil)
+  (setq lsp-keep-workspace-alive nil)
+  (setq read-process-output-max (* 1024 1024)) ;; 1MB
+  (setq lsp-idle-delay 0.25)
+  (setq lsp-auto-execute-action nil))
 
-;; org
-(require 'org)
-(setq org-clock-sound "~/Documents/emacs/ding.wav")
+(use-package lsp-ui
+  :commands lsp-ui-mode
+  :custom-face
+  (lsp-ui-sideline-global ((t (:italic t))))
+  (lsp-ui-peek-highlight  ((t (:foreground unspecified :background unspecified :inherit isearch))))
+  :config
+  (setq lsp-ui-doc-enable nil)
+  (setq lsp-ui-doc-show-with-mouse nil)
+  (setq lsp-ui-doc-enhanced-markdown nil)
+  (setq lsp-ui-doc-delay 0.01)
+  (when (display-graphic-p)
+    (setq lsp-ui-doc-use-childframe t)
+    (setq lsp-ui-doc-text-scale-level -1.0)
+    (setq lsp-ui-doc-max-width 80)
+    (setq lsp-ui-doc-max-height 25)
+    (setq lsp-ui-doc-position 'at-point))
+  (setq lsp-ui-doc-include-signature t)
+  (setq lsp-ui-doc-border (face-foreground 'font-lock-comment-face))
+  (setq lsp-ui-sideline-diagnostic-max-line-length 80)
+  (setq lsp-ui-sideline-diagnostic-max-lines 2)
+  (setq lsp-ui-peek-always-show t)
+  (setq lsp-ui-sideline-delay 0.05))
 
-;; Evil-Nerd-commenter
+;; Python
+(use-package lsp-pyright
+  :hook (python-mode . (lambda () (require 'lsp-pyright)))
+  :init (when (executable-find "python3")
+          (setq lsp-pyright-python-executable-cmd "python3")))
+;; Java
+(use-package lsp-java
+  :after lsp)
+
+(use-package java
+  :ensure nil
+  :after lsp-java
+  :bind (:map java-mode-map ("C-c i" . lsp-java-add-import)))
+
+(use-package company
+  :hook (prog-mode . company-mode)
+  :config
+  (setq company-idle-delay 0.0)
+  (setq company-tooltip-minimum-width 60)
+  (setq company-tooltip-maximum-width 60)
+  (setq company-tooltip-limit 7)
+  (setq company-minimum-prefix-length 1)
+  (setq company-tooltip-align-annotations t)
+  (setq company-frontends '(company-pseudo-tooltip-frontend ; show tooltip even for single candidate
+                            company-echo-metadata-frontend))
+  (unless (display-graphic-p)
+    (define-key company-active-map (kbd "C-h") #'backward-kill-word)
+    (define-key company-active-map (kbd "C-w") #'backward-kill-word))
+  (define-key company-active-map (kbd "C-j") nil) ; avoid conflict with emmet-mode
+  (define-key company-active-map (kbd "C-n") #'company-select-next)
+  (define-key company-active-map (kbd "C-p") #'company-select-previous)
+  (if (display-graphic-p)
+      (define-key company-active-map (kbd "<tab>") 'company-select-next)
+    (define-key company-active-map (kbd "TAB") 'company-select-next))
+  (define-key company-active-map (kbd "<backtab>") 'company-select-previous))
+
+(use-package company-prescient
+  :after (prescient company)
+  :config
+  (company-prescient-mode +1))
+
+(use-package company-box
+  :if (display-graphic-p)
+  :hook (company-mode . company-box-mode)
+  :config
+  (setq company-box-doc-enable nil)
+  (setq company-box-scrollbar nil)
+  (setq company-box-frame-behavior 'default))
+
+(use-package flycheck
+  :hook ((prog-mode . flycheck-mode)
+         (markdown-mode . flycheck-mode)
+         (org-mode . flycheck-mode))
+  :custom-face
+  (flycheck-error   ((t (:inherit error :underline t))))
+  (flycheck-warning ((t (:inherit warning :underline t))))
+  :config
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (setq flycheck-display-errors-delay 0.1)
+  (setq-default flycheck-disabled-checkers '(python-pylint))
+  (setq flycheck-flake8rc "~/.config/flake8")
+  (setq flycheck-checker-error-threshold 1000)
+  (setq flycheck-indication-mode nil)
+  (define-key flycheck-mode-map (kbd "<f8>") #'flycheck-next-error)
+  (define-key flycheck-mode-map (kbd "<S-f8>") #'flycheck-previous-error)
+  (flycheck-define-checker proselint
+    "A linter for prose. Install the executable with `pip3 install proselint'."
+    :command ("proselint" source-inplace)
+    :error-patterns
+    ((warning line-start (file-name) ":" line ":" column ": "
+	      (id (one-or-more (not (any " "))))
+	      (message) line-end))
+    :modes (markdown-mode org-mode))
+  (add-to-list 'flycheck-checkers 'proselint))
+
+(use-package lua-mode)
+
+(use-package json-mode)
+
+(use-package vimrc-mode)
+
+(use-package yaml-mode)
+
+;; icons
+(use-package all-the-icons
+  :ensure t
+  :config
+  (setq all-the-icons-scale-factor 0.8))
+
+;; TODO WHY DOING
+(use-package hl-todo
+  :custom-face
+  (hl-todo                        ((t (:inverse-video nil :italic t :bold nil))))
+  :config
+  (add-to-list 'hl-todo-keyword-faces '("DOING" . "#94bff3"))
+  (add-to-list 'hl-todo-keyword-faces '("WHY" . "#7cb8bb"))
+  (global-hl-todo-mode +1))
+
+(use-package xclip
+  :unless (display-graphic-p)
+  :config
+  (xclip-mode +1))
+
+;; Dashboard
+(use-package dashboard
+  :ensure t
+  :config
+  (dashboard-setup-startup-hook)
+  (setq dashboard-startup-banner 'logo)
+  (setq dashboard-banner-logo-title "( E M A C S )")
+  (setq dashboard-init-info "")
+  (setq dashboard-items nil)
+  (setq dashboard-set-footer t)
+  (setq dashboard-footer-icon "")
+  ;; (setq dashboard-footer-messages '("😈 Happy hacking!   "))
+  (define-key dashboard-mode-map (kbd "<f5>") #'(lambda ()
+                                                  (interactive)
+                                                  (dashboard-refresh-buffer)
+                                                  (message "Refreshing Dashboard...done"))))
+
+
+;; Evil-Nerd2-commenter
 (use-package evil-nerd-commenter
   :init
   (evilnc-default-hotkeys nil t))
 (global-set-key (kbd "M-;") 'evilnc-comment-or-uncomment-lines)
 
+(use-package minions
+  :config
+  (setq minions-mode-line-lighter "")
+  (setq minions-mode-line-delimiters '("" . ""))
+  (setq-default mode-line-buffer-identification '("%b // " (:eval (projectile-project-name))))
+  (minions-mode +1))
+
+(use-package org
+  :hook ((org-mode . visual-line-mode)
+         (org-mode . auto-fill-mode)
+         (org-mode . org-indent-mode)
+         (org-mode . (lambda ()
+                                              (setq-local olivetti-body-width (+ fill-column 5)))))
+  :config
+  (require 'org-tempo)
+  (setq org-link-descriptive nil)
+  (setq org-startup-folded nil)
+  (setq org-todo-keywords '((sequence "TODO" "DOING" "DONE")))
+  (add-to-list 'org-file-apps '("\\.pdf\\'" . emacs))
+  (setq org-html-checkbox-type 'html))
+
+(use-package org-bullets
+  :hook (org-mode . org-bullets-mode))
+
 ;; Aggressive-indent
-(add-hook 'emacs-lisp-mode-hook #'aggressive-indent-mode)
+;; (add-hook 'emacs-lisp-mode-hook #'aggressive-indent-mode)
 
 ;; undo fu & undo fu session
 
@@ -102,10 +313,93 @@
   (global-set-key (kbd "C-z")   'undo-fu-only-undo)
   (global-set-key (kbd "C-S-z") 'undo-fu-only-redo))
 
-;; helm
-(use-package helm
+;; Git
+(use-package git-gutter
+  :hook (prog-mode . git-gutter-mode)
+  :ensure t
   :init
-  (helm-mode 1))
+  (global-git-gutter-mode +1))
+
+;; Git integration
+
+(use-package magit
+  :bind ("C-x g" . magit-status)
+  :config
+  (local-unset-key (kbd "f"))
+  (define-key magit-mode-map (kbd "<f5>") #'(lambda ()
+                                              (interactive)
+                                              (magit-refresh)
+                                              (message "Refreshing Magit...done"))))
+
+;; format
+(use-package format-all
+  :preface
+  (defun format-code ()
+    "Auto-format whole buffer."
+    (interactive)
+    (let ((windowstart (window-start)))
+      (if (derived-mode-p 'prolog-mode)
+          (prolog-indent-buffer)
+        (format-all-buffer))
+      (set-window-start (selected-window) windowstart)))
+  (defalias 'format-document #'format-code)
+  :config
+  (global-set-key (kbd "C-f") #'format-code)
+  (add-hook 'prog-mode-hook #'format-all-ensure-formatter)
+  (add-hook 'python-mode-hook #'(lambda ()
+                                  (setq-local format-all-formatters '(("Python" yapf)))))
+  (add-hook 'sql-mode-hook #'(lambda ()
+                               (setq-local format-all-formatters '(("SQL" pgformatter))))))
+
+;; ivy
+(use-package ivy
+  :hook (after-init . ivy-mode)
+  :config
+  (setcdr (assoc t ivy-format-functions-alist) #'ivy-format-function-line)
+  (setq ivy-height 15)
+  (setq ivy-display-style nil)
+  (setq ivy-re-builders-alist
+        '((counsel-rg            . ivy--regex-plus)
+          (counsel-projectile-rg . ivy--regex-plus)
+          (swiper                . ivy--regex-plus)
+          (t                     . ivy--regex-fuzzy)))
+  (setq ivy-use-virtual-buffers t)
+  (setq ivy-count-format "(%d/%d) ")
+  (setq ivy-initial-inputs-alist nil)
+  (if (display-graphic-p)
+      (progn
+        (define-key ivy-minibuffer-map (kbd "<tab>") #'ivy-next-line)
+        (define-key ivy-minibuffer-map (kbd "<return>") #'ivy-alt-done)
+        (define-key ivy-minibuffer-map (kbd "<C-return>") #'ivy-immediate-done))
+    (progn
+      (define-key ivy-minibuffer-map (kbd "TAB") #'ivy-next-line)
+      (define-key ivy-minibuffer-map (kbd "RET") #'ivy-alt-done)
+      (define-key ivy-minibuffer-map (kbd "C-j") #'ivy-immediate-done)))
+  (define-key ivy-minibuffer-map (kbd "<backtab>") #'ivy-previous-line)
+  (define-key ivy-minibuffer-map (kbd "C-c m") #'ivy-mark)
+  (define-key ivy-minibuffer-map (kbd "C-c u") #'ivy-unmark))
+
+
+;; swiper
+(use-package swiper
+  :after ivy
+  :config
+  (setq swiper-action-recenter t)
+  (setq swiper-goto-start-of-match t))
+
+;; ivy-prescient
+(use-package ivy-prescient
+  :after (prescient ivy counsel)
+  :config
+  (setq ivy-prescient-sort-commands
+        '(:not swiper
+               counsel-grep
+               counsel-rg
+               counsel-projectile-rg
+               ivy-switch-buffer
+               counsel-switch-buffer))
+  (setq ivy-prescient-retain-classic-highlighting t)
+  (ivy-prescient-mode +1))
 
 ;; Theme
 (load-theme 'gruber-darker t)
@@ -130,7 +424,6 @@
   (transpose-lines 1)
   (forward-line -1))
 
-
 (global-set-key (kbd "<M-up>") 'move-line-up)
 (global-set-key (kbd "<M-down>") 'move-line-down)
 
@@ -142,4 +435,3 @@
                   (line-beginning-position (+ 1 arg)))
   (message "%d line%s copied" arg (if (= 1 arg) "" "s")))
 (global-set-key "\C-c\C-c" 'copy-line)
-
