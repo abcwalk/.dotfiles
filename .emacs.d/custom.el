daf89f1366b4d1b29524e28a83e32d917c3a14b8
behemothbucket <mahagon.here@gmail.com>
2023-09-28 10:00:23 +0300

    fix: return to modus themes for `main` branch

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -16,7 +16,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(mode-line ((t :background unspecified :box unspecified :overline "#595959")))
+ '(mode-line-active ((t :inherit mode-line :box unspecified)))
+ '(mode-line-inactive ((t :background unspecified :foreground "#595959" :box unspecified :overline "#595959"))))
 
 (provide 'custom)
 ;;; custom.el ends here
