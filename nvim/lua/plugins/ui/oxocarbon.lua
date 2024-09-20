30367dfd67a23938658a848c6193ce8b79bd7f45
behemothbucket <mahagon.here@gmail.com>
2024-09-20 13:05:14 +0300

    update

--- nvim/lua/plugins/ui/oxocarbon.lua
+++ nvim/lua/plugins/ui/oxocarbon.lua
@@ -4,6 +4,8 @@ vim.o.background = 'dark'
 vim.cmd('colorscheme oxocharcoal')
 
 local white = '#d0d0d0'
+local green = '#99ff99'
+local orange = '#ffb897'
 
 vim.api.nvim_set_hl(0, 'variable', { fg = white })
 vim.api.nvim_set_hl(0, 'FloatBorder', { fg = white })
@@ -17,4 +19,6 @@ vim.api.nvim_set_hl(0, 'TelescopePromptTitle', { fg = oxocharcoal.base02, bg = o
 vim.api.nvim_set_hl(0, 'TelescopeResultsTitle', { fg = oxocharcoal.blend, bg = oxocharcoal.blend })
 vim.api.nvim_set_hl(0, 'TelescopeSelection', { fg = oxocharcoal.none, bg = oxocharcoal.base02 })
 vim.api.nvim_set_hl(0, 'TelescopePreviewLine', { fg = oxocharcoal.none, bg = oxocharcoal.base01 })
-vim.api.nvim_set_hl(0, 'DiagnosticHint', { fg = '#99ff99' })
+vim.api.nvim_set_hl(0, 'DiagnosticHint', { fg = green })
+vim.api.nvim_set_hl(0, 'GitsignsAdd', { fg = green })
+vim.api.nvim_set_hl(0, 'GitsignsChange', { fg = orange })
